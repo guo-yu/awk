21059e33793b75d8408f1d234d4e0c9924eabcbd
Guo Yu <i@guoyu.me>
2014-07-19 21:52:57 +0800

    1.0: add entrypoint

---
+++ index.js
@@ -0,0 +1,9 @@
+//                   __  
+//   ____ __      __/ /__
+//  / __ `/ | /| / / //_/
+// / /_/ /| |/ |/ / ,<   
+// \__,_/ |__/|__/_/|_|  
+//
+// @brief: a wrapper of [WebAwk](https://github.com/agordon/webawk)
+
+module.exports = require('./libs/awk');
